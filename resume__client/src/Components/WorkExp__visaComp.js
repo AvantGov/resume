ccf24e26a305d161a202cf643065082d55bf192f
avantgov <alexcouts18@gmail.com>
2021-04-10 19:52:45 -0500

    check point: scoping development needs for more information pages

--- resume__client/src/Components/WorkExp__visaComp.js
+++ resume__client/src/Components/WorkExp__visaComp.js
@@ -7,7 +7,8 @@ const WorkExp__visaComp = () => {
             <h2 className='visaComp__header'> 💳 Global Product Management Intern</h2>
             <p className='visaComp__desc'>After receiving immersive security product training, covering authentication and authorization SFTP for eCommerce, my role owned primarily copywriting and inter-team coordination and reporting assignments. With these tasks at the core focus of my days, I proposed and delivered an enhanced in-house tool that provided GPM team members with an operable dashboard to track and resolve customer requests reported by implementation service team members. </p>
             <a className='visaComp__more' href='/'>More Info</a>
-            {/*  TODO: add router content in separate component for learn more information */} 
+            {/*  TODO: add router content in separate component for learn more information */}
+
         </div>
     )
 };
