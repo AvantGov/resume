965a653c439ed59ecac58106295d333da056dc16
avantgov <alexcouts18@gmail.com>
2021-04-12 16:27:07 -0500

    added some view more content infrastructure

--- resume__client/src/Components/EpicComp__viewmore.js
+++ resume__client/src/Components/EpicComp__viewmore.js
@@ -12,6 +12,29 @@ const EpicComp__viewmore = () => {
                 <p className='intro__desc'>QA Analyst on MyChart "Cerberus" Framework & Interoperability team. This team takes broad ownership of Account Activation and Two-Factor Authorization, Proxy Access connection, and web-service Branding Customization along side low-level supporting framework upgrades. Core duties include ensuring functional excellence of dataflows between client and intermediary web servers through manual workflow testing and thorough reporting on software quality levels, while following up with RnD channels to source and resolve found issues. </p>
             </div>
 
+            <div className='viewmore__ownership'>
+                <h1 className='ownership__title'>Ownership</h1>
+                <div className='ownership__container'>
+                    <div className='ownContainer__areaContainer'>
+                        <h2 className='areaContainer__title'>Title</h2>
+                        <p className='areaContainer__desc'>i am the descriptio hear me roar</p>
+                    </div>
+                    
+                    <div className='ownContainer__areaContainer'>
+                        <h2 className='areaContainer__title'>Title</h2>
+                        <p className='areaContainer__desc'>i am the descriptio hear me roar</p>
+                    </div>
+                    
+                    <div className='ownContainer__areaContainer'>
+                        <h2 className='areaContainer__title'>Title</h2>
+                        <p className='areaContainer__desc'>i am the descriptio hear me roar</p>
+                    </div>                    
+                </div>
+            </div>
+
+            <div className='viewmore__technical'>
+                i will be the technical concepts component
+            </div>
 
             <div className='viewmore__return'>
                     <Link to="/">
