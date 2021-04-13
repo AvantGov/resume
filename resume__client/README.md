292111dc0dc7b1eb1856a4c9314df900be5c44e8
avantgov <alexcouts18@gmail.com>
2021-04-12 22:48:03 -0500

    added component architecture for skills section on homepage with sub components that will be rendered as viewmore pages | wrote data schema for stored content objects to paginate view more pages with

--- resume__client/README.md
+++ resume__client/README.md
@@ -1,10 +1,13 @@
 TODOs: 
-- add content to the epic view more page 
 - architect skills section 
 - create component page to hold all other activities 
 - create component page for education 
 - create contact page 
     - possible to be done with pop up instead 
+- considering having the additional roles and skills and education section added as a route switch with a component that will render at the bottom of the page if the user would just like to switch between them at the end of each page 
+    - will it still have the "take me back?" 
 
 ENHANCEMENTS: 
 - add make viewmore pages just pop ups instead, unless on oa mobile device then they will be shown as other pages. 
+
+- figure out the issue with the user not being returned to the top of the page / find a way to force teh user back to the top of the page without forcing a reload of the page. 
---
+++ resume__client/src/assets/data/skills.js
@@ -0,0 +1,60 @@
+const skills__programming = {
+    hello: "the value of value of the programming object"
+}
+
+const skills__media = {
+    hello: "the value of the media object"
+}
+
+const skills__ecomm = {
+    hello: "the value of the ecomm object   "
+}
+
+export { skills__programming, skills__ecomm, skills__media };
+
+
+// ! data map of object for each skills section 
+
+// * PROGRAMMING 
+// const skills__programming = {
+//     LANG: {
+//         name: "",
+//         rating: 4,
+//         description: "",
+//         hasLink: true,
+//         linkURL: "",
+//         linkName: "",
+//         hasLib: true,
+//         lib: {
+//             LIB: {
+//                 name: "",
+//                 description: "",
+//                 hasLink: true,
+//                 linkURL: "",
+//                 linkName: ""
+//             }
+//         }
+//     }
+// }
+
+// * MEDIA 
+// const skills__media = {
+//     TOOL: {
+//         name: "",
+//         rating: 4,
+//         decription: "",
+//         linkURL: "",
+//         linkName: "",
+//     }
+// }
+
+// * ECOMM
+// const skills__ecomm = {
+//     TOOL: {
+//         name: "",
+//         rating: 4,
+//         decription: "",
+//         linkURL: "",
+//         linkName: "",
+//     }
+// } 
