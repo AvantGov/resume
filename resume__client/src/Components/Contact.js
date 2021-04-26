1d9bd9453556ee090e2fc5bf0244cd52c62aa24a
avantgov <alexcouts18@gmail.com>
2021-04-26 02:06:20 -0500

    updated playlist link in the footer section

--- resume__client/src/Components/Contact.js
+++ resume__client/src/Components/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
                         <a href='https://www.linkedin.com/in/alex-c-329721109/' target='_blank' rel="noreferrer" className='item__link'>LinkedIn</a>
                     </li>
                     <li className='unlist__item'>
-                        <a href='https://open.spotify.com/playlist/0e9k3vanb4UYGLIPzFddot?si=Lgkx0A51QS-jnDw76LLXWA' target='_blank' rel="noreferrer" className='item__link'>Spotify Playlist</a>
+                        <a href='https://open.spotify.com/playlist/6a6WHsODpZRCD4lUwyYVdN?si=QZOoZ-MYQYOc7T-8ch-4Kw' target='_blank' rel="noreferrer" className='item__link'>Spotify Playlist</a>
                     </li>
                 </ul>
             </div>
