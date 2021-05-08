360539af87c7813310597fa85d7022b4ae51d283
avantgov <alexcouts18@gmail.com>
2021-05-08 18:59:17 -0500

    added component files and basic arc for recent work sections and home page items

---
+++ client/src/Components/RecentWork.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RecentWork = () => {
+
+
+    return(
+        <div className='recentWork'>
+            I am the recent work page
+        </div>
+    )
+};
+
+export default RecentWork;
---
+++ client/src/Components/RecentWorkHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const RecentWorkHeader = () => {
+
+
+    return(
+        <div className='recentWorkHeader'>
+            I am the recent work header look at me! 
+        </div>
+    )
+};
+
+export default RecentWorkHeader;
